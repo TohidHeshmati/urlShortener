176929f1b88cf0a61f5f7553fa42122fc5356e46
Tohid Heshmati <tohidfla@gmail.com>
2025-07-10 19:01:26 +0200

    init application

---
+++ src/main/kotlin/com/tohid/url_shortner/HomeController.kt
@@ -0,0 +1,12 @@
+package com.tohid.url_shortner
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/")
+class HomeController {
+    @GetMapping ("home")
+    fun home(): String {
+        return "Welcome to the URL Shortener Service!"
+    }
+}
---
+++ src/main/kotlin/com/tohid/url_shortner/UrlShortnerApplication.kt
@@ -0,0 +1,11 @@
+package com.tohid.url_shortner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlShortnerApplication
+
+fun main(args: Array<String>) {
+    runApplication<UrlShortnerApplication>(*args)
+}
