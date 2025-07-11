4bb4b589f2ba4676f2f522b654068a6c1379240f
Tohid Heshmati <tohidfla@gmail.com>
2025-07-11 22:33:03 +0200

    create a job to delete expired urls

--- src/main/kotlin/com/tohid/url_shortener/repository/UrlRepository.kt
+++ src/main/kotlin/com/tohid/url_shortener/repository/UrlRepository.kt
@@ -1,11 +1,22 @@
 package com.tohid.url_shortener.repository
 
 import com.tohid.url_shortener.domain.Url
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import java.time.Instant
 
 @Repository
 interface UrlRepository : CrudRepository<Url, Long> {
     fun findByShortUrl(shortUrl: String): Url?
     fun findByOriginalUrl(originalUrl: String): Url?
+
+    @Transactional
+    @Modifying
+    @Query(
+        "DELETE FROM Url u WHERE u.expiryDate IS NOT NULL AND u.expiryDate < :time",
+    )
+    fun deleteByExpiryDateBefore(time: Instant): Int
 }
